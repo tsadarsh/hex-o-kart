b201e13983fa579ce7e7b18313aa27e6bbea8e26
Adarsh TS <tsadarsh0707@gmail.com>
2022-06-11 17:17:49 +0530

    [Doc]: Update doc abount second encoder

--- encoder/encoder_esp32/encoder_esp32.ino
+++ encoder/encoder_esp32/encoder_esp32.ino
@@ -19,7 +19,7 @@
    pinMode (left_enc_B,INPUT);
    
    Serial.begin (9600);
-   // Reads the initial state of the outputA
+   // Reads the initial state of the A and B of both the encoder
    right_aLastState = digitalRead(right_enc_A);
    left_aLastState = digitalRead(left_enc_A);   
    attachInterrupt(digitalPinToInterrupt(right_enc_A), right_update, CHANGE);
@@ -41,10 +41,10 @@
  }
  
  void right_update() {
-    right_aState = digitalRead(right_enc_A); // Reads the "current" state of the outputA
-   // If the previous and the current state of the outputA are different, that means a Pulse has occured
+    right_aState = digitalRead(right_enc_A); // Reads the "current" state of the A
+   // If the previous and the current state of the A are different, that means a Pulse has occured
    if (right_aState != right_aLastState){     
-     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+     // If the outputB state is different to the A state, that means the encoder is rotating clockwise
      if (digitalRead(right_enc_B) != right_aState) { 
        right_counter ++;
      } else {
@@ -55,10 +55,10 @@
  }
 
   void left_update() {
-    left_aState = digitalRead(left_enc_A); // Reads the "current" state of the outputA
-   // If the previous and the current state of the outputA are different, that means a Pulse has occured
+    left_aState = digitalRead(left_enc_A); // Reads the "current" state of the A
+   // If the previous and the current state of the A are different, that means a Pulse has occured
    if (left_aState != left_aLastState){     
-     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
+     // If the outputB state is different to the A state, that means the encoder is rotating clockwise
      if (digitalRead(left_enc_B) != left_aState) { 
        left_counter ++;
      } else {
