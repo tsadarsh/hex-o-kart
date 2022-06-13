91c00ea9f49316604a1e2d06c9865ee8a7e60135
Adarsh TS <tsadarsh0707@gmail.com>
2022-06-13 22:31:04 +0530

    [Refactor]: To integrate with `controller.py`

--- gui.py
+++ gui.py
@@ -1,31 +1,34 @@
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber')
 
-FORWARD = '^'
-STOP    = 'O'
-REVERSE = 'v'
+class Gui:
+	def __init__(self):
+		sg.theme('DarkAmber')
+		self.FORWARD = '10'
+		self.STOP    = '00'
+		self.REVERSE = '01'
+		self.STATUS = '00'
 
-layout = [
-	[
-		sg.Button(FORWARD),
-		sg.Button(STOP),
-		sg.Button(REVERSE),
-		sg.Button('Cancel')	
-	]
-]
+	def start(self):
+		layout = [
+			[
+				sg.Button(self.FORWARD),
+				sg.Button(self.STOP),
+				sg.Button(self.REVERSE),
+				sg.Button('Cancel')
+			]
+		]
 
-window = sg.Window('GUI', layout)
+		window = sg.Window('GUI', layout)
 
-while True:
-	event, values = window.read()
-	if event == sg.WIN_CLOSED or event == 'Cancel':
-		break
-	if event == FORWARD:
-		print("Going forward")
-	if event == STOP:
-		print("Stopping")
-	if event == REVERSE:
-		print("Going reverse")
+		while True:
+			event, values = window.read()
+			if event == sg.WIN_CLOSED or event == 'Cancel':
+				self.kill(window)
+				break
+			else:
+				self.STATUS = event
+				print(self.STATUS)
 
-window.close()
+	def kill(self, window):
+		window.close()
