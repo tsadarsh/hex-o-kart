2cd9a5a9e785f1e5c3e2dba683c4bf09ab241baa
Adarsh TS <tsadarsh0707@gmail.com>
2022-06-13 22:59:00 +0530

    [Add]: Integrate GUI

--- controller.py
+++ controller.py
@@ -1,14 +1,25 @@
 import paho.mqtt.client as mqtt
 from random import randint
+import time
 
-client_id = 'user_'+str(randint(1, 1000000000))+'tsa'
+from gui import Gui
 
-client = mqtt.Client(client_id=client_id)
+gui = Gui()
+w = gui.create_window()
+
+def create_unique_client_id():
+	client_id = 'user_'+str(randint(1, 1000000000))+'_tsa'
+	return client_id
+
+client = mqtt.Client(client_id=create_unique_client_id())
 client.connect("broker.mqttdashboard.com")
 
-message = "python"+client_id
-client.publish("/swa/commands", message)
-# Loop needed as per https://stackoverflow.com/a/47726934/12808184
-client.loop()
+while gui.READY:
+	gui.event_loop(w)
+	message = gui.STATUS
+	client.publish("/swa/commands", message)
+	# Loop needed as per https://stackoverflow.com/a/47726934/12808184
+	client.loop()
 
-print(f"{client_id} is publishing: {message}")
+	print(f"publishing: {message}")
+	time.sleep(0.1)
