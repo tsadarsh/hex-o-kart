f84195547cce20f66ed80b6bca06d4dbc0151ed2
Adarsh TS <tsadarsh0707@gmail.com>
2022-06-16 12:30:17 +0530

    [Fix]: Poll subscribe continously to avoid starting new thread while using `poll_start()`

--- esp_server_gui.py
+++ esp_server_gui.py
@@ -8,16 +8,16 @@ window = gui.create_window()
 c = Controller()
 c.connect()
 
-c.subscribe(
-	[
-		"/swa/encoder/left",
-		"/swa/encoder/right"
-	]
-)
 
 while gui.READY:
 	gui.event_loop(window)
 	message = gui.MESSAGE
 	c.publish(message)
+	c.subscribe(
+		[
+			"/swa/encoder/left",
+			"/swa/encoder/right"
+		]
+	)
 	gui.left_encoder = c.left_encoder
 	gui.right_encoder = c.right_encoder
