48e92946c576b1f6b171ef1307946e4fdc201159
Adarsh TS <tsadarsh0707@gmail.com>
2022-06-16 12:29:46 +0530

    [Fix]: Make loop as non blocking

--- controller.py
+++ controller.py
@@ -25,27 +25,32 @@ class Controller:
 		client_id = 'user_'+str(randint(1, 1000000000))+'_tsa'
 		return client_id
 
+	def loop_start(self):
+		self.client.loop_start()
+
 	def publish(self, message):
 		self.client.publish("/swa/commands", message)
 		# Loop needed as per https://stackoverflow.com/a/47726934/12808184
-		self.client.loop()
+		self.client.loop(.1)
 
-		print(f"publishing: {message}")
-		time.sleep(0.1)
+		#print(f"publishing: {message}")
+		#time.sleep(0.1)
 
 	def subscribe(self, topics: list):
 		"""Subscribes to topics in `topics` argument"""
 		for topic in topics:
 			self.client.subscribe(topic)
 		self.client.on_message = self._on_message
-		self.client.loop_start()
+		self.client.loop(.1)
 
 	def _on_message(self, client, userdata, message):
 		"""Callback function of `client.subscribe`"""
 		if message.topic == "/swa/encoder/left":
-			self.left_encoder = str(message.payload.decode("utf-8"))
-			
+			#print(message.payload)
+			self.left_encoder = message.payload.decode("utf-8")
+
 		if message.topic == "/swa/encoder/right":
-			self.right_encoder = str(message.payload.decode("utf-8"))
+			#print(message.payload)
+			self.right_encoder = message.payload.decode("utf-8")
 			
 
