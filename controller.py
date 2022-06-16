51428ddcda0c0bc4951fb76a6b0e2acc3ce06ca2
Adarsh TS <tsadarsh0707@gmail.com>
2022-06-16 10:28:29 +0530

    [Fix]: Controller now prints received msg in stout

--- controller.py
+++ controller.py
@@ -2,9 +2,8 @@ import paho.mqtt.client as mqtt
 from random import randint
 import time
 
-from gui import Gui
 
-class Sender:
+class Controller:
 	"""Class to publish message to MQTT broker
 
 	First connect to server by calling `connect` method. Then publish
@@ -12,13 +11,19 @@ class Sender:
 	as argument. 
 	"""
 
-	def _create_unique_client_id(self):
-		client_id = 'user_'+str(randint(1, 1000000000))+'_tsa'
-		return client_id
+	def __init__(self):
+		self.left_encoder = 0
+		self.right_encoder = 0
 
 	def connect(self):
+		"""Create a client and connects to MQTT broker"""
 		self.client = mqtt.Client(client_id=self._create_unique_client_id())
 		self.client.connect("broker.mqttdashboard.com")
+
+	def _create_unique_client_id(self):
+		"""Creates unique client ID to connect to MQTT broker"""
+		client_id = 'user_'+str(randint(1, 1000000000))+'_tsa'
+		return client_id
 
 	def publish(self, message):
 		self.client.publish("/swa/commands", message)
@@ -28,5 +33,19 @@ class Sender:
 		print(f"publishing: {message}")
 		time.sleep(0.1)
 
-class Receiver:
-	pass
+	def subscribe(self, topics: list):
+		"""Subscribes to topics in `topics` argument"""
+		for topic in topics:
+			self.client.subscribe(topic)
+		self.client.on_message = self._on_message
+		self.client.loop_start()
+
+	def _on_message(self, client, userdata, message):
+		"""Callback function of `client.subscribe`"""
+		if message.topic == "/swa/encoder/left":
+			self.left_encoder = str(message.payload.decode("utf-8"))
+			print(self.left_encoder)
+		if message.topic == "/swa/encoder/right":
+			self.right_encoder = str(message.payload.decode("utf-8"))
+			print(self.right_encoder)
+
